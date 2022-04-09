66c14ebb13c31ff209fe75632b6f4502d38074b8
Steven Cardenas <stevenco200@gmail.com>
2022-04-09 16:42:31 -0500

    Creamos un archivo para los tipos de funciones

---
+++ funciones.js
@@ -0,0 +1,20 @@
+//Declarativas
+
+function miFuncion () {
+
+    return 3;
+}
+
+miFuncion();
+
+//Expresion
+
+let miFuncion = function(a,b){
+    return a + b;
+}
+
+miFuncion();
+
+function saludarEstudiantes (estudiantes){
+
+}
