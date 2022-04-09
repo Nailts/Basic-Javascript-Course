0604c243f9b793a257de3991f4cbeea771f0990e
Steven Cardenas <stevenco200@gmail.com>
2022-04-09 13:05:41 -0500

    Creamos un archivo para ejemplificar la declaracion y la inicializacion de una variable

---
+++ variables.js
@@ -0,0 +1,19 @@
+let nombre = "Steven ";
+
+// Declarar
+let edad;
+
+//Inicializar
+edad = 30;
+
+edad
+
+let elementos = ["PC", "Celular"];
+
+let persona = {
+    nombres: "Steven ",
+    edad: 22    
+}
+
+persona
+ 
