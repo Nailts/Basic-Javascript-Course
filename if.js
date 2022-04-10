22216aa0a35426c481ce9db44e95b83a972ae3fd
Steven Cardenas <stevenco200@gmail.com>
2022-04-09 20:36:02 -0500

    Se creo el archivo dela clase 1 de condicionales anexando un ejercicio de if/else

---
+++ if.js
@@ -0,0 +1,48 @@
+    let edad = 18;
+
+    if (edad===18){
+        console.log("Este ciudadano puede votar por primera vez");
+    }else if(edad>18){
+        console.log("Este ciudadano ya ha votado");
+    }else {
+        console.log("Aun no puedes votar");
+    }
+
+    let numero = 1;
+
+    let resultado = numero == 1 ?  "Soy un uno " : "No soy un uno ";
+
+
+    //-----------Reto piedra papel o tijeras-------------//
+
+    let jugador1;
+    let jugador2;
+
+               
+
+        if (jugador1 = "Piedra" && jugador2 == "Tijeras") {
+
+            console.log("Piedra vence tijeras: El jugador 1 gana");         
+        } 
+
+        else if (jugador1 = "Piedra" && jugador2 == "Papel") {
+                        
+            console.log("Papel vence tijeras: El jugador 2 gana");         
+        }
+
+        else if (jugador1 = "Piedra" && jugador2 == "Piedra") {
+                        
+            console.log("Es un empate");         
+        }
+        else if (jugador1 = "Piedra" && jugador2 == "Papel") {
+                        
+            console.log("Papel vence tijeras: El jugador 2 gana");         
+        }
+
+        else if (jugador1 = "Piedra" && jugador2 == "Piedra") {
+                        
+            console.log("Es un empate");         
+        } 
+
+    
+
