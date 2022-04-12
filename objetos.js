2c06b049b9d98b140f4c555bcb0af2d52c162b9f
Steven Cardenas <stevenco200@gmail.com>
2022-04-11 21:02:11 -0500

    Se complemento la clase de objetos con la ejecucion de la funcion constructiva

--- objetos.js
+++ objetos.js
@@ -14,3 +14,22 @@ console.log(miAuto); //Accedemos a las propiedades del objeto
 console.log(miAuto.marca);//Con este metodo accdemos a una propiedad especifica del objeto
 
 miAuto.detalleDelAuto();
+
+//-------------Funcion constructora----------------------//
+
+function auto(marca, modelo, annio) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;
+}
+
+//-------------Ejecutando la funcion conbstructiva "auto-----------//"
+
+let autoNuevo = new auto("Tesla","Model 3","2020");    //<------new es un objeto que deriva de otro objeto 
+
+console.log(autoNuevo); //<-----Muestra el nuevo objeto derivado de la funcion constructiva "auto"
+
+let autoNuevo2 = new auto("Tesla", "Model x", "2018");
+let autoNuevo3 = new auto("Toyota", "Corolla", "2020");
+
+console.log(autoNuevo2);
